fddb15b17b552a451f69f5169cfecdcd3fb72016
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-04-20 11:40:02 +0330

    Sync code with video

--- django-getting-started/urls.py
+++ django-getting-started/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('welcome.urls')),
-    path('upload/', include('upload.urls')),
+    path('welcome/', include('welcome.urls')),
+    path('', include('upload.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
