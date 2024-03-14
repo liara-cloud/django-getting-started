66379cd827808d0687710dcb8b1ba7686bafb33b
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-03-14 12:47:30 +0330

    show uploaded image after uploading

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_photo
+from .views import upload_photo, show_uploaded_photo
 
 urlpatterns = [
     path('', upload_photo, name='upload_photo'),
+    path('show/', show_uploaded_photo, name='show_uploaded_photo'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -2,13 +2,18 @@
 
 from django.shortcuts import render, redirect
 from .forms import PhotoForm
+from .models import Photo
 
 def upload_photo(request):
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('upload_photo')
+            return redirect('show_uploaded_photo')
     else:
         form = PhotoForm()
     return render(request, 'photos/upload_photo.html', {'form': form})
+
+def show_uploaded_photo(request):
+    latest_photo = Photo.objects.last()  # Retrieve the latest uploaded photo
+    return render(request, 'photos/show_uploaded_photo.html', {'photo': latest_photo})
