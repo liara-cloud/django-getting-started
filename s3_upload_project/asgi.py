16bfdb1437d25497203a0f7806aceb322ca695ab
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-03-14 11:34:39 +0330

    init project

---
+++ s3_upload_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for s3_upload_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 's3_upload_project.settings')
+
+application = get_asgi_application()
