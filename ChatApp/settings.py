c12d53ad32dcf1451c1bb6034ad2ef4d2cde3445
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-03-18 11:17:24 +0330

    add chat app to settings.py

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne', 
+    'chat', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels', 
+    'channels',
 ]
 
 ASGI_APPLICATION = 'ChatApp.asgi.application'
