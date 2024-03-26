a9aea83d79afd7581c92488eb6c959a60c7c61d7
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-03-26 13:07:44 +0330

    add connectionpool

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+import random
+import string
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
+class TestModel(models.Model):
+    data = models.CharField(max_length=100)
+
+    @classmethod
+    def generate_random_data(cls):
+        data = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        cls.objects.create(data=data)
 
     def __str__(self):
-        return self.name
+        return self.data
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from psycopg_pool import ConnectionPool
+import random, string
 
-# Create your views here.
+def index(request):
+    with ConnectionPool(conninfo='postgresql://root:cIdX6SQawIaWScOKpFBcel1v@olympus.liara.cloud:34464/postgres') as pool:
+        with pool.connection() as conn:
+            with conn.cursor() as cursor:
+                for _ in range(10):
+                    data = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+                    cursor.execute("INSERT INTO myapp_testmodel (data) VALUES (%s)", [data])
+
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT data FROM myapp_testmodel")
+                data = [row[0] for row in cursor.fetchall()]
+    
+    return render(request, 'myapp/index.html', {'data': data})
