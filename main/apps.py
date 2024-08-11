f6179602ee1d7525ac744f33bf689ec72f119f12
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-08-11 11:27:32 +0330

    update to django version 5

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WelcomeConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'welcome'
+    name = 'main'
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'welcome.html')
+    return render(request, 'index.html')
